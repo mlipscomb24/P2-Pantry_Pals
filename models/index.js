493aa70a5c61e154c4dcb769b559e0607e0c44c7
Daniel D <ddunnemann@gmail.com>
2024-08-20 19:41:20 -0500

    Item model and dashboard controller

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
+const Item = require('./Item');
+
+User.hasMany(Item, {
+    foreignKey: 'item_id',
+});
+
+Item.belongTo(User, {
+    foreignKey: 'item_id',
+});
 
 module.exports = { User };
+
